a9765d1edf10a53e41e96a912eeaf569ade5f975
Péntek László <pentek.laszlo1994@gmail.com>
2024-03-18 09:01:22 +0100

    form validation

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,6 +1,7 @@
 let guessListContainer = document.querySelector(".js-guess-container");
 let currentGuessField = document.querySelector(".js-guess-field");
 let newGameButton = document.querySelector(".js-new-game-button");
+let guessSubmitButton = document.querySelector(".js-submit-guess");
 
 let target;
 
@@ -20,7 +21,40 @@ function startGame(){
     target = generateTarget();
 }
 
+function submitGuess(event){
+    event.preventDefault();
+    let guessValue = Number.parseInt(currentGuessField.value);
+    console.log(guessValue, typeof guessValue);
+    if (validateGuess()){
+        alert("Klikk");
+    }
+}
+
+function addGuessValidation(){
+    currentGuessField.classList.add("is-invalid");
+}
+
+function removeValidation(){
+    currentGuessField.classList.remove("is-invalid");
+}
+
+function validateGuess(){
+    let guessValue = Number.parseInt(currentGuessField.value);
+    currentGuessField.value = guessValue;
+    if(Number.isNaN(guessValue) || guessValue < 1 || guessValue > 100){
+        addGuessValidation();
+        return false;
+    }
+        removeValidation();
+        return true;
+}
+
 startGame();
 newGameButton.addEventListener("click", startGame);
+guessSubmitButton.addEventListener("click", submitGuess);
+currentGuessField.addEventListener("focus", removeValidation)
+currentGuessField.addEventListener("change", validateGuess)
+
+
 
 console.log(generateTarget())
